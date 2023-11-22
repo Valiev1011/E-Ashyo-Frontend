d31ad0ff946f0089e13ac767e3e03c8e184cdaed
valiev1011 <valievyunus1011@gmail.com>
2023-11-22 09:18:54 +0500

    few changes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <TheNavbar></TheNavbar>
+    <MasonryBrand></MasonryBrand>
     <router-view></router-view>
+    <MasonryCategory></MasonryCategory>
     <TheFooter></TheFooter>
   </div>
 </template>
@@ -9,6 +11,8 @@
 <script setup lang="ts">
 import TheNavbar from "../components/header/TheNavbar.vue";
 import TheFooter from "../components/footer/TheFooter.vue";
+import MasonryBrand from "../components/ui/MasonryBrand.vue";
+import MasonryCategory from "../components/ui/MasonryCategory.vue";
 </script>
 
 <style scoped></style>
