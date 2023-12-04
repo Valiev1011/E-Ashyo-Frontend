9991f54a17c4d891bf61c66ac65dfec04bc0f055
samandarlv <134990329+samandarlv@users.noreply.github.com>
2023-12-04 17:17:43 +0500

    auto play none

--- src/pages/product/viewProductPage.vue
+++ src/pages/product/viewProductPage.vue
@@ -4,7 +4,6 @@
       Last viewed products
     </h1>
     <Carousel
-      :autoplay="3000"
       :pause-autoplay-on-hover="true"
       v-bind="settings"
       :breakpoints="breakpoints"
@@ -21,121 +20,120 @@
 </template>
 
 <script setup lang="ts">
-import VCard from "../../components/form/VCard.vue";
-import { onMounted, ref } from "vue";
-// @ts-ignore
-import { Carousel, Navigation, Slide } from "vue3-carousel";
-import "vue3-carousel/dist/carousel.css";
-import { useAdminProductStore } from "../../store/products";
+  import VCard from "../../components/form/VCard.vue";
+  import { onMounted, ref } from "vue";
+  import { Carousel, Navigation, Slide } from "vue3-carousel";
+  import "vue3-carousel/dist/carousel.css";
+  import { useAdminProductStore } from "../../store/products";
 
-const store = useAdminProductStore();
+  const store = useAdminProductStore();
 
-const products = ref();
+  const products = ref();
 
-onMounted(async () => {
-  console.log(store, "store...");
+  onMounted(async () => {
+    console.log(store, "store...");
 
-  products.value = await store.lastViewedProducts();
-});
+    products.value = await store.lastViewedProducts();
+  });
 
-const breakpoints = ref({
-  // 700px and up
-  700: {
-    itemsToShow: 3.3,
+  const breakpoints = ref({
+    // 700px and up
+    700: {
+      itemsToShow: 3.3,
+      snapAlign: "center",
+    },
+    // 1024 and up
+    1024: {
+      itemsToShow: 5.0,
+      snapAlign: "start",
+    },
+  });
+
+  const settings = ref({
+    itemsToShow: 1,
     snapAlign: "center",
-  },
-  // 1024 and up
-  1024: {
-    itemsToShow: 5.0,
-    snapAlign: "center",
-  },
-});
-
-const settings = ref({
-  itemsToShow: 0.5,
-  snapAlign: "center",
-});
-const items = ref([
-  {
-    image: "http://localhost:5173/src/assets/airpods.png",
-    title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
-    price: "6 999 999 uzs",
-    lifetime: "6 oy / 1 200 000 usz",
-    sale: true,
-  },
-  {
-    image: "http://localhost:5173/src/assets/airpods.png",
-    title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
-    price: "6 999 999 uzs",
-    lifetime: "6 oy / 1 200 000 usz",
-    sale: false,
-  },
-  {
-    image: "http://localhost:5173/src/assets/airpods.png",
-    title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
-    price: "6 999 999 uzs",
-    lifetime: "6 oy / 1 200 000 usz",
-    sale: true,
-  },
-  {
-    image: "http://localhost:5173/src/assets/airpods.png",
-    title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
-    price: "6 999 999 uzs",
-    lifetime: "6 oy / 1 200 000 usz",
-    sale: true,
-  },
-  {
-    image: "http://localhost:5173/src/assets/airpods.png",
-    title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
-    price: "6 999 999 uzs",
-    lifetime: "6 oy / 1 200 000 usz",
-    sale: true,
-  },
-  {
-    image: "http://localhost:5173/src/assets/airpods.png",
-    title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
-    price: "6 999 999 uzs",
-    lifetime: "6 oy / 1 200 000 usz",
-    sale: true,
-  },
-  {
-    image: "http://localhost:5173/src/assets/airpods.png",
-    title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
-    price: "6 999 999 uzs",
-    lifetime: "6 oy / 1 200 000 usz",
-    sale: false,
-  },
-  {
-    image: "http://localhost:5173/src/assets/airpods.png",
-    title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
-    price: "6 999 999 uzs",
-    lifetime: "6 oy / 1 200 000 usz",
-    sale: true,
-  },
-  {
-    image: "http://localhost:5173/src/assets/airpods.png",
-    title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
-    price: "6 999 999 uzs",
-    lifetime: "6 oy / 1 200 000 usz",
-    sale: false,
-  },
-  {
-    image: "http://localhost:5173/src/assets/airpods.png",
-    title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
-    price: "6 999 999 uzs",
-    lifetime: "6 oy / 1 200 000 usz",
-    sale: false,
-  },
-]);
+  });
+  const items = ref([
+    {
+      image: "http://localhost:5173/src/assets/airpods.png",
+      title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
+      price: "6 999 999 uzs",
+      lifetime: "6 oy / 1 200 000 usz",
+      sale: true,
+    },
+    {
+      image: "http://localhost:5173/src/assets/airpods.png",
+      title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
+      price: "6 999 999 uzs",
+      lifetime: "6 oy / 1 200 000 usz",
+      sale: false,
+    },
+    {
+      image: "http://localhost:5173/src/assets/airpods.png",
+      title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
+      price: "6 999 999 uzs",
+      lifetime: "6 oy / 1 200 000 usz",
+      sale: true,
+    },
+    {
+      image: "http://localhost:5173/src/assets/airpods.png",
+      title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
+      price: "6 999 999 uzs",
+      lifetime: "6 oy / 1 200 000 usz",
+      sale: true,
+    },
+    {
+      image: "http://localhost:5173/src/assets/airpods.png",
+      title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
+      price: "6 999 999 uzs",
+      lifetime: "6 oy / 1 200 000 usz",
+      sale: true,
+    },
+    {
+      image: "http://localhost:5173/src/assets/airpods.png",
+      title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
+      price: "6 999 999 uzs",
+      lifetime: "6 oy / 1 200 000 usz",
+      sale: true,
+    },
+    {
+      image: "http://localhost:5173/src/assets/airpods.png",
+      title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
+      price: "6 999 999 uzs",
+      lifetime: "6 oy / 1 200 000 usz",
+      sale: false,
+    },
+    {
+      image: "http://localhost:5173/src/assets/airpods.png",
+      title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
+      price: "6 999 999 uzs",
+      lifetime: "6 oy / 1 200 000 usz",
+      sale: true,
+    },
+    {
+      image: "http://localhost:5173/src/assets/airpods.png",
+      title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
+      price: "6 999 999 uzs",
+      lifetime: "6 oy / 1 200 000 usz",
+      sale: false,
+    },
+    {
+      image: "http://localhost:5173/src/assets/airpods.png",
+      title: "Смартфон Xiaomi 12 Lite 8/128Gb Қора kamera 48/68 px",
+      price: "6 999 999 uzs",
+      lifetime: "6 oy / 1 200 000 usz",
+      sale: false,
+    },
+  ]);
 </script>
 
 <style>
-.carousel__icon {
-  width: var(--vc-icn-width);
-  height: var(--vc-icn-width);
-  fill: currentColor;
-  padding: 1px;
-  background: white;
-  border-radius: 50%;
-}
+  .carousel__icon {
+    width: var(--vc-icn-width);
+    height: var(--vc-icn-width);
+    fill: currentColor;
+    padding: 1px;
+    background: white;
+    border-radius: 50%;
+  }
 </style>
